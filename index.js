4ab6f39876d351a16554840e9bdd66029a7b8192
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 18:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = new Object();
 
 // Returns an object with the original key value pairs and the new key value pair
+// It does not modify theoriginal object, but rather returns a clone with the new data
 function updateObjectWithKeyAndValue(object, key, value) {
   var tempObject = new Object();
   tempObject[key] = value;
